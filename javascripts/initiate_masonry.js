806b3e5e7c420d10a1382e20b4b3e8874f79d181
Sushil Shrestha <sushil10018@gmail.com>
2017-12-09 14:46:25 +0545

    git initiated

---
+++ javascripts/initiate_masonry.js
@@ -0,0 +1,8 @@
+var container = document.querySelector('.blog_list .container');
+var msnry = new Masonry( container, {
+	itemSelector: '.blog_each'
+});
+
+var msnry = new Masonry( '.blog_list .container', {
+  // options
+});
